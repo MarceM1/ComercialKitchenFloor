efa9f809fdb6286e13edf8bcdea4f8edfc2d45aa
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-08-29 16:12:11 -0300

    Home terminado

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.toright', {
+    duration: 5000,
+    origin: 'right',
+    distance: '-150px'
+});
+sr.reveal('.toleft', {
+    duration: 5000,
+    origin: 'left',
+    distance: '-150px'
+});
+sr.reveal('.todown', {
+    duration: 5000,
+    origin: 'bottom',
+    distance: '-150px'
+});
+sr.reveal('.toup', {
+    duration: 5000,
+    origin: 'top',
+    distance: '-150px'
+});
