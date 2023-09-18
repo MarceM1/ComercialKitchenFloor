63b279f76c9cd8fa64b377d687b4345ce25448c2
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-09-17 21:02:57 -0300

    contact us section

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,28 @@
 window.sr = ScrollReveal();
 
 sr.reveal('.toright', {
-    duration: 5000,
+    duration: 3000,
     origin: 'right',
     distance: '-150px'
 });
 sr.reveal('.toleft', {
-    duration: 5000,
+    duration: 3000,
     origin: 'left',
     distance: '-150px'
 });
 sr.reveal('.todown', {
-    duration: 5000,
+    duration: 3000,
     origin: 'bottom',
     distance: '-150px'
 });
 sr.reveal('.toup', {
-    duration: 5000,
+    duration: 3000,
     origin: 'top',
     distance: '-150px'
 });
+sr.reveal('.fastUp', {
+    duration: 2500,
+    origin: "top",
+    distance: '-50px',
+    
+}) 
